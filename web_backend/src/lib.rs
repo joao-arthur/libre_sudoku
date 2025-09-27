18a07b8864b5fb3161f91bf41190baf5e8bb2fb9
joao-arthur <joao.lothamer@gmail.com>
2025-09-27 14:54:54 -0300

    feat(web_backend): draw sudoku table

---
+++ web_backend/src/lib.rs
@@ -0,0 +1,88 @@
+use wasm_bindgen::{prelude::wasm_bindgen, JsCast};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
+use std::cell::RefCell;
+
+#[derive(Debug, PartialEq)]
+pub struct Model {
+    pub dim: u16
+}
+
+thread_local! {
+    static MODEL: RefCell<Model> = RefCell::new(Model { dim: 0 });
+}
+
+#[wasm_bindgen(js_name = "engineInit")]
+pub fn main_init(canvas: HtmlCanvasElement) {
+    let min = 0.0;
+    let len = 500.0;
+    let height = 3.0;
+
+    if let Ok(Some(context)) = canvas.get_context("2d") {
+        if let Ok(context2d) = context.dyn_into::<CanvasRenderingContext2d>() {
+            context2d.set_fill_style(&"#f8f8f8".into());
+            context2d.fill_rect(0.0, 0.0, len, len);
+            context2d.set_stroke_style(&"#333".into());
+            context2d.set_line_width(3.0);
+
+            let mid_min = height / 2.0;
+            let mid_max = len - height / 2.0;
+
+            context2d.begin_path();
+            context2d.move_to(mid_min, mid_min);
+            context2d.line_to(mid_min, mid_max);
+            context2d.line_to(mid_max, mid_max);
+            context2d.line_to(mid_max, mid_min);
+            context2d.line_to(mid_min, mid_min);
+            context2d.stroke();
+
+            context2d.set_stroke_style(&"#555".into());
+            context2d.set_line_width(2.0);
+
+            
+
+            context2d.begin_path();
+            context2d.move_to(0.0, len / 3.0);
+            context2d.line_to(len, len / 3.0);
+            context2d.stroke();
+
+            context2d.begin_path();
+            context2d.move_to(0.0, len / 3.0 * 2.0);
+            context2d.line_to(len, len / 3.0 * 2.0);
+            context2d.stroke();
+    
+            context2d.begin_path();
+            context2d.move_to(len / 3.0, 0.0);
+            context2d.line_to(len / 3.0, len);
+            context2d.stroke();
+
+            context2d.begin_path();
+            context2d.move_to(len / 3.0 * 2.0, 0.0);
+            context2d.line_to(len / 3.0 * 2.0, len);
+            context2d.stroke();
+    
+
+
+
+
+
+        }
+    }
+}
+
+fn print_table(context2d: &CanvasRenderingContext2d, min: f64, max: f64) {
+    context2d.set_fill_style(&"#333".into());
+
+    context2d.fill_rect(max / 3.0, min, min + 1.0, max);
+    context2d.fill_rect(max / 3.0 * 2.0, min, min + 1.0, max);
+
+    context2d.fill_rect(min, max / 3.0, max, min + 1.0);
+    context2d.fill_rect(min, max / 3.0 * 2.0, max, min + 1.0);
+}
+
+#[wasm_bindgen(js_name = "engineSetDimension")]
+pub fn main_set_dim(dim: u16) {
+    MODEL.with(|i| {
+        let mut m = i.borrow_mut();
+        m.dim = dim;
+    });
+}
--- web_frontend/src/routes/+page.svelte
+++ web_frontend/src/routes/+page.svelte
@@ -1,18 +1,48 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import initWASM, { engineInit, engineSetDimension } from "sudoku_engine";
+
+    let initiated = $state(false);
     let innerWidth = $state(0);
     let innerHeight = $state(0);
 
-    function onKeyPress(e: KeyboardEvent) {
+    let canvas: HTMLCanvasElement;
 
-    }
+    onMount(() => {
+        if (!initiated) {
+            initWASM().then(() => {
+                initiated = true;
+                let context = canvas.getContext("2d");
+                if (!context) {
+                    return;
+                }
+                engineInit(canvas);
+                engineSetDimension(Math.min(innerWidth, innerHeight));
+            });
+        }
+    });
 </script>
 
-<svelte:window onkeypress={onKeyPress} bind:innerWidth bind:innerHeight />
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <style>
- 
+    main {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+    }
+
+    canvas {
+        margin: auto;
+    }
 </style>
 
 <main>
-
+    <canvas
+        bind:this={canvas}
+        width={Math.min(innerHeight, innerWidth)}
+        height={Math.min(innerHeight, innerWidth)}
+        style={`width: ${Math.min(innerHeight, innerWidth)}px; height: ${Math.min(innerHeight, innerWidth)}px;`}
+    >
+    </canvas>
 </main>
